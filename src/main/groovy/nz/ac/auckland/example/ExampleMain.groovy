6229a067f77a8e693172a2e0d3333261f220b287
Richard Vowles <richard@bluetrainsoftware.com>
2014-09-04 09:45:16 +1200

    Initial commit of Basic Groovy App

---
+++ src/main/groovy/nz/ac/auckland/example/ExampleMain.groovy
@@ -0,0 +1,10 @@
+package nz.ac.auckland.example
+
+/**
+ * @author Richard Vowles - https://google.com/+RichardVowles
+ */
+class ExampleMain {
+	public static void main(String[] args) {
+		println "Hello World"
+	}
+}
---
+++ src/test/groovy/nz/ac/auckland/example/ExampleMainTest.groovy
@@ -0,0 +1,14 @@
+package nz.ac.auckland.example
+
+import org.junit.Test
+
+/**
+ * @author Richard Vowles - https://google.com/+RichardVowles
+ */
+class ExampleMainTest {
+
+	@Test
+	public void basicTest() {
+		ExampleMain.main([] as String[])
+	}
+}
